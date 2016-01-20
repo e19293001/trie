6cb7ad7f188d436195efd0e2149d38d2c464f78a
e19293001 <e19293001@gmail.com>
2016-01-20 21:21:43 +0800

    initial version

---
+++ TernarySearchTrie.c
@@ -0,0 +1,6 @@
+#include "TernarySearchTrie.h"
+tstNode* tstInsert(tstNode* p, char *s) {
+  if (p == NULL) {
+    p = (tstNode *) malloc(sizeof(tstNode));
+  }                                     
+}
---
+++ testTstNode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "unitTestNode.h"
+
+int main() {
+  TestTernarySearchTrie();
+  return 0;
+}
