336d63f73b207ba868a40c01c1284e6b101f9a9b
e19293001 <e19293001@gmail.com>
2016-02-03 20:06:23 +0800

    added more test for token manager. partially working

--- testTSTParseSymtab.c
+++ testTSTParseSymtab.c
@@ -2,7 +2,55 @@
 
 void Test0000Parser() {
   TokenManager* tm;
+  Token tok;
   tm = TokenManagerNew("tst/testpattern_tokenmanager.txt");
   //TokenManagerGetNextToken(&tm);
+//  while (tok.kind != _EOF) {
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+//  }
+
+    tok = TokenManagerGetNextToken(&tm);
+  printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
+    tok = TokenManagerGetNextToken(&tm);
+    printf("token.image: %s\n", tok.image);
+
   TokenManagerDelete(tm);
 }
