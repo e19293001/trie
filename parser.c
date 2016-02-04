e80288cf1745f1c556e9d79bbd92d842269dd628
e19293001 <e19293001@gmail.com>
2016-02-04 21:27:04 +0800

    building the grammar. need to add implement advance()

--- parser.c
+++ parser.c
@@ -1,5 +1,50 @@
 #include "parser.h"
 
+parserData* ParserNew() {
+  parserData *ret;
+
+  ret = malloc(sizeof(parserData));
+
+  ret->tm = TokenManagerNew("tst/testpattern_parser0000.txt");
+
+  return ret;
+}
+
+void ParserStart(parserData *t) {
+  _parser = t;
+  _parser->currentToken = TokenManagerGetNextToken(&_parser->tm);
+  if (t->currentToken.kind == PUSH) {
+    printf("call push\n");
+    consume(PUSH);
+    push();
+  }
+  else if (t->currentToken.kind == PUSHC) {
+    printf("call pushc\n");
+  }
+  else if (t->currentToken.kind == PUSHWC) {
+    printf("call pushwc\n");
+  }
+  else if (t->currentToken.kind == HALT) {
+    printf("call halt\n");
+  }
+  else {
+    printf("error unknown token\n");
+  }
+}
+
+void push() {
+  //if (_parser->currentToken
+}
+
+void pushc() {
+}
+
+void pushwc() {
+}
+
+void halt() {
+}
+
 void ParserConsume() {
 }
 
@@ -10,4 +55,19 @@ Token ParserGetToken() {
 }
 
 void ParserAdvance() {
+}
+
+void consume(int expected) {
+  if (_parser->currentToken.kind != expected) {
+    ParserAdvance();
+  }
+  else {
+    printf("Found token \"%s\", ", _parser->currentToken.image);
+    printf("expecting kind of \"%s\"\n", tokenImage[expected]);
+  }
+}
+
+void ParserDelete(parserData *t) {
+  TokenManagerDelete(t->tm);
+  free(t);
 }
--- parser.h
+++ parser.h
@@ -1,9 +1,28 @@
 #ifndef parser_h
 #define parser_h
+
 #include "tokenmanager.h"
 
+typedef struct parserData {
+  TokenManager *tm;
+  Token currentToken;
+  Token previousToken;
+} parserData;
+
+parserData *_parser;
+
+parserData* ParserNew();
 void ParserConsume();
 Token ParserGetToken();
 void ParserAdvance();
+void ParserDelete(parserData *t);
+void ParserStart(parserData *t);
+
+void push();
+void pushc();
+void pushwc();
+void halt();
+
+void consume(int expected);
 
 #endif
