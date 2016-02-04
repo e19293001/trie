601cf840f3691515aa2834b0fad3506884112972
e19293001 <e19293001@gmail.com>
2016-02-04 21:24:10 +0800

    added call to Test0001Parser for testing the parser

--- maintestTSTParseSymtab.c
+++ maintestTSTParseSymtab.c
@@ -1,6 +1,7 @@
 #include "testTSTParseSymtab.h"
 
 int main() {
-  Test0000Parser(); // test token manager
+  //Test0000Parser(); // test token manager
+  Test0001Parser(); // test parser
   return 0;
 }
