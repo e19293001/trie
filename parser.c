1ff5d33b6d7984b6a45f9837a3b1a329d1a6a552
e19293001 <e19293001@gmail.com>
2016-02-04 20:34:20 +0800

    initial version

---
+++ parser.c
@@ -0,0 +1,13 @@
+#include "parser.h"
+
+void ParserConsume() {
+}
+
+Token ParserGetToken() {
+  Token ret;
+
+  return ret;
+}
+
+void ParserAdvance() {
+}
---
+++ parser.h
@@ -0,0 +1,9 @@
+#ifndef parser_h
+#define parser_h
+#include "tokenmanager.h"
+
+void ParserConsume();
+Token ParserGetToken();
+void ParserAdvance();
+
+#endif
