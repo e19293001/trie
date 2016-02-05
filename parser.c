7f395ab253d2592ad913a08805d7493aafb9bb5d
e19293001 <e19293001@gmail.com>
2016-02-05 09:49:50 +0800

    made the parser to work. next step: integrate tst as a symbol table

--- parser.c
+++ parser.c
@@ -6,26 +6,45 @@ parserData* ParserNew() {
   ret = malloc(sizeof(parserData));
 
   ret->tm = TokenManagerNew("tst/testpattern_parser0000.txt");
+  ret->currentToken.next = NULL;
+  ret->previousToken.next = NULL;
 
   return ret;
 }
 
 void ParserStart(parserData *t) {
   _parser = t;
+  program(t);
+  if (_parser->currentToken.kind != _EOF) {
+    printf("expecting EOF. token is: %s\n", tokenImage[_parser->currentToken.kind]);
+    exit(-1);
+  }
+}
+
+void program() {
   _parser->currentToken = TokenManagerGetNextToken(&_parser->tm);
-  if (t->currentToken.kind == PUSH) {
+  if (_parser->currentToken.kind == PUSH) {
     printf("call push\n");
-    consume(PUSH);
     push();
+    program();
   }
-  else if (t->currentToken.kind == PUSHC) {
+  else if (_parser->currentToken.kind == PUSHC) {
     printf("call pushc\n");
+    pushc();
+    program();
   }
-  else if (t->currentToken.kind == PUSHWC) {
+  else if (_parser->currentToken.kind == PUSHWC) {
     printf("call pushwc\n");
+    pushwc();
+    program();
   }
-  else if (t->currentToken.kind == HALT) {
+  else if (_parser->currentToken.kind == HALT) {
     printf("call halt\n");
+    halt();
+    program();
+  }
+  else if (_parser->currentToken.kind == _EOF) {
+    // do nothing
   }
   else {
     printf("error unknown token\n");
@@ -33,19 +52,30 @@ void ParserStart(parserData *t) {
 }
 
 void push() {
-  //if (_parser->currentToken
+  if (_parser->currentToken.kind == PUSH) {
+    consume(PUSH);
+    consume(UNSIGNED);
+  }
 }
 
 void pushc() {
+  if (_parser->currentToken.kind == PUSHC) {
+    consume(PUSHC);
+    consume(UNSIGNED);
+  }
 }
 
 void pushwc() {
+  if (_parser->currentToken.kind == PUSHWC) {
+    consume(PUSHWC);
+    consume(UNSIGNED);
+  }
 }
 
 void halt() {
-}
-
-void ParserConsume() {
+  if (_parser->currentToken.kind == HALT) {
+    consume(HALT);
+  }
 }
 
 Token ParserGetToken() {
@@ -55,6 +85,14 @@ Token ParserGetToken() {
 }
 
 void ParserAdvance() {
+  _parser->previousToken = _parser->currentToken;
+
+  if (_parser->currentToken.next != NULL) {
+    _parser->currentToken = *(_parser->currentToken.next);
+  }
+  else {
+    _parser->currentToken = TokenManagerGetNextToken(&_parser->tm);
+  }
 }
 
 void consume(int expected) {
@@ -62,6 +100,7 @@ void consume(int expected) {
     ParserAdvance();
   }
   else {
+    printf("consume: %s\n", tokenImage[expected]);
     printf("Found token \"%s\", ", _parser->currentToken.image);
     printf("expecting kind of \"%s\"\n", tokenImage[expected]);
   }
